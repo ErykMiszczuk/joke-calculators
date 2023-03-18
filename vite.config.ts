54fbfda0761d2ab63141ce46aacf56f70be854d7
Eryk Miszczuk <eryk.miszczuk1996@gmail.com>
2023-03-18 14:29:24 +0100

    Modified build conf

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/joke-calculators/',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
